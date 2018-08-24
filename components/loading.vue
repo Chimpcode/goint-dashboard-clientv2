efd55710669db77aacfbb9c3b65ee28bf0d2d9c7
anargu <anthony.arostegui@gmail.com>
2018-08-23 21:07:16 -0500

    loading component

---
+++ components/loading.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div class="loading-page" v-if="loading">
+    <p>Loading...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    loading: false
+  }),
+  methods: {
+    start () {
+      this.loading = true
+    },
+    finish () {
+      this.loading = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.loading-page {
+  position: fixed;
+  z-index: 50;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(232, 76, 84, 0.733);
+  text-align: center;
+  padding-top: 200px;
+  font-size: 30px;
+  font-family: sans-serif;
+}
+</style>
